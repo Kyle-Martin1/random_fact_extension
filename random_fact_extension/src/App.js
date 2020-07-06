298d467d02de8cc0de1dba960efb77324e78bbf9
Kyle Martin <kylemartin1403@hotmail.co.uk>
2020-07-06 17:01:29 +0100

    Updates

--- random_fact_extension/src/App.js
+++ random_fact_extension/src/App.js
@@ -4,9 +4,22 @@ import styles from './App.module.css';
 import FactContent from './Components/FactContent/FactContent.js';
 
 class App extends Component {
+
+    state = {
+        backgroundColor: ['FD151B', '339989', 'B2FFD6', '6ABEA7', 'C1666B', 'F17F29', 'CA1551'],
+        randomNumber: null
+    }
+
+    componentDidMount(){
+        let randomNumber = Math.floor(Math.random() * 7);
+        this.setState({
+            randomNumber: randomNumber
+        })
+    }
+
     render(){
         return(
-            <div className={styles.mainContainer}>
+            <div className={styles.mainContainer} style={{backgroundColor: '#' + this.state.backgroundColor[this.state.randomNumber]}}>
                 <FactContent />
             </div>
         )
