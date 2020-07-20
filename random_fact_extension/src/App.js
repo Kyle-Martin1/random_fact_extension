09ee811a2b6fb996044fb0e0f9e75b63dd6f10e1
Kyle Martin <kylemartin1403@hotmail.co.uk>
2020-07-20 17:52:47 +0100

    Updates

--- random_fact_extension/src/App.js
+++ random_fact_extension/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Draggable, { DraggableCore } from 'react-draggable';
 
 import styles from './App.module.css';
 import FactContent from './Components/FactContent/FactContent.js';
@@ -7,7 +8,10 @@ class App extends Component {
 
     state = {
         backgroundColor: ['FD151B', '339989', 'B2FFD6', '6ABEA7', 'C1666B', 'F17F29', 'CA1551'],
-        randomNumber: null
+        randomNumber: null,
+        deltaPosition: {
+            x: 0, y: 0
+        }
     }
 
     componentDidMount(){
@@ -17,11 +21,35 @@ class App extends Component {
         })
     }
 
+    handleDrag = (e, ui) => {
+        const {x, y} = this.state.deltaPosition;
+        this.setState({
+            deltaPosition: {
+                x: x + ui.deltaX,
+                y: y + ui.deltaY,
+            }
+        });
+    };
+
+    onStart = () => {
+        this.setState({activeDrags: ++this.state.activeDrags});
+    };
+
+    onStop = () => {
+      this.setState({activeDrags: --this.state.activeDrags});
+    };
+
     render(){
+
+        const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+        const {deltaPosition, controlledPosition} = this.state;
+
         return(
-            <div className={styles.mainContainer} style={{backgroundColor: '#' + this.state.backgroundColor[this.state.randomNumber]}}>
-                <FactContent />
-            </div>
+            <Draggable {...dragHandlers}>
+                <div className={styles.mainContainer} style={{backgroundColor: '#' + this.state.backgroundColor[this.state.randomNumber]}}>
+                    <FactContent />
+                </div>
+            </Draggable>
         )
     }
 }
